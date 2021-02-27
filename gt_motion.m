607550b2ac4fed3ff9abbb0a8fa6609270bfefe4
Ciaran Donegan <donegaci@tcd.ie>
2021-02-27 10:25:56 +0000

    Refactoring code. Nikalus motion is calculated in the script instead of reading in saved motion .mat

--- gt_motion.m
+++ gt_motion.m
@@ -1,20 +1,24 @@
 close all 
 clear
 
-% data = 'parkrun_data/';
-% files = dir(fullfile(data, '*.mat'));
-load data/memc/parkrun/frame_0010.mat
-load motion.mat
-load parkrun_gt_flow_010_i_011.mat
-
+% Frame to inspect
 frame_num = 10;
 
+% Load in saved motion vectors 
+load (sprintf('data/memc/parkrun/frame_%04d.mat', frame_num))
+load (sprintf('data/niklaus/parkrun/frame_%04d.mat', frame_num))
+load (sprintf('data/ground_truth/parkrun/gt_forw_frame_%04d.mat', frame_num))
+
+% tidy up variables
+memc_motion = motion;
+clear('motion', 'img_interp', 'img', 'occlusion', 'filter')
+
 % read in picture
-name = sprintf('./HD_dataset/HD720p_GT/parkrun_frames/frame_%03d.png', frame_num-1);
+name = sprintf('data/ground_truth/parkrun/frame_%04d.png', frame_num-1);
 prev_frame = imread(name);
-name = sprintf('./HD_dataset/HD720p_GT/parkrun_frames/frame_%03d.png', frame_num+1);
+name = sprintf('data/ground_truth/parkrun/frame_%04d.png', frame_num+1);
 next_frame = imread(name);
-name = sprintf('./HD_dataset/HD720p_GT/parkrun_frames/frame_%03d.png', frame_num);
+name = sprintf('data/ground_truth/parkrun/frame_%04d.png', frame_num);
 gt_frame = imread(name);
 
 % define some place you want to look at
@@ -25,13 +29,38 @@ sx = 590 : 660;
 gt_u_forward = gt_flow(:,:,1);
 gt_v_forward = gt_flow(:,:,2);
 
-% optic flow vectors
-forw_motion = squeeze(motion(2,:,:,:));
-u_forward = squeeze(forw_motion(:,:,1));
-v_forward = squeeze(forw_motion(:,:,2));
-back_motion = squeeze(motion(1,:,:,:));
-u_back = squeeze(back_motion(:,:,1));
-v_back = squeeze(back_motion(:,:,2));
+
+% MEMC optic flow vectors
+forw_memc = squeeze(memc_motion(2,:,:,:)); % forward motion
+u_memc = squeeze(forw_memc(:,:,1)); % x-component
+v_memc = squeeze(forw_memc(:,:,2)); % y-component
+
+
+% NIKLAUS
+dim = size(vert1, 3, 4);
+back_niklaus = zeros([2, dim]);
+forw_niklaus = zeros([2, dim]);
+
+for x = sx
+    for y = sy
+        % matrix multiplication vertical by horizontal
+        % transpose the vertical vector
+        kernel1 = vert1(1,:,y,x)' * hor1(1,:,y,x);
+        kernel2 = vert2(1,:,y,x)' * hor2(1,:,y,x);
+        
+
+        % kernel1
+        [CoM_x1, CoM_y1, CoM_vector1] = getCenterOfMass(kernel1);
+        [max_abs_x1, max_abs_y1, max_abs_vector1] = getMaxAbs(kernel1);
+        % kernel2
+        [CoM_x2, CoM_y2, CoM_vector2] = getCenterOfMass(kernel2);
+        [max_abs_x2, max_abs_y2, max_abs_vector2] = getMaxAbs(kernel2);
+
+        back_niklaus(:, y, x) = CoM_vector1;
+        forw_niklaus(:, y, x) = CoM_vector2;
+
+    end
+end
 
 figure()
 % subplot(1,2,1)
@@ -55,12 +84,12 @@ X = ones(length(sy), 1) * sx;
 Y = sy' * ones(1, length(sx));
 n = 4;
 quiver(X(1 : n : end, 1 : n : end), Y(1 : n : end, 1 : n : end),...
-  u_forward(sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end)),...
-  v_forward(sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end)), 0, 'r-',...
+  u_memc(sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end)),...
+  v_memc(sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end)), 0, 'r-',...
   'linewidth', 2.5);
 quiver(X(1 : n : end, 1 : n : end), Y(1 : n : end, 1 : n : end),...
-        squeeze(motion_forw(1, sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end))),...
-        squeeze(motion_forw(2, sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end))), 0, 'b-',...
+        squeeze(forw_niklaus(1, sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end))),...
+        squeeze(forw_niklaus(2, sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end))), 0, 'b-',...
         'linewidth', 2.5)
 quiver(X(1 : n : end, 1 : n : end), Y(1 : n : end, 1 : n : end),...
   gt_u_forward(sy(1 : n : end, 1 : n : end), sx(1 : n : end, 1 : n : end)),...
@@ -69,20 +98,4 @@ quiver(X(1 : n : end, 1 : n : end), Y(1 : n : end, 1 : n : end),...
  legend(["MEMC-NET", "Niklaus", "GT (RAFT)"])
  axis image
 
-
-forw_occl = im2uint8(squeeze(occlusion(2,:,:)));
-
-figure()
-gray_img = overlayed(:,:,1);
-image(sx, sy, gray_img(sy, sx));
-colormap(gray(256))
-axis image
-
-covered = forw_occl - 127;
-uncovered = 127 - forw_occl;
-
-rgb = cat(3, gray_img+uncovered, gray_img, gray_img+covered);
-figure()
-image(rgb(sy, sx, :));
-axis image
 
